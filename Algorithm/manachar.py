267521498c7fd202c974a0393e1e72617d224bdb
中二的愚者 <397504018@qq.com>
2020-05-07 19:50:19 +0800

    manachar

---
+++ Algorithm/manachar.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# author:lyh
+# datetime:2020/5/7 19:07
+"""
+manachar
+最长回文子串
+"""
+
+
+def manachar(s: str) -> str:
+    temp = "#"
+    for c in s:
+        temp += c + "#"
+    maxl = [0 for i in range(len(temp))]
+    maxr = -1
+    mid = -1
+    res = 0
+    for i in range(len(temp)):
+        maxl[i] = maxl[2 * mid - i] if i <= maxr else 0
+        while i + maxl[i] + 1 < len(temp) and i - maxl[i] - 1 >= 0 and temp[i + maxl[i] + 1] == temp[i - maxl[i] - 1]:
+            maxl[i] += 1
+        if maxl[i] > maxr:
+            maxr = maxl[i]
+            mid = i
+        res = max(res, mid[i])
+    return res
