4cb9986ee0ff61796d4e22b6a665c754d2536eee
lyhshang <397504018@qq.com>
2020-05-13 10:16:36 +0800

    0039

---
+++ Solutions/0039.combinationSum.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+# author:lyh
+# datetime:2020/5/13 8:41
+"""
+39. 组合总和
+
+给定一个无重复元素的数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
+
+candidates 中的数字可以无限制重复被选取。
+
+说明：
+
+    所有数字（包括 target）都是正整数。
+    解集不能包含重复的组合。
+
+示例 1:
+
+输入: candidates = [2,3,6,7], target = 7,
+所求解集为:
+[
+  [7],
+  [2,2,3]
+]
+
+示例 2:
+
+输入: candidates = [2,3,5], target = 8,
+所求解集为:
+[
+  [2,2,2,2],
+  [2,3,3],
+  [3,5]
+]
+"""
+from typing import List
+
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        self.l = len(candidates)
+        self.candidates = candidates
+        self.res = []
+        self.path = []
+        self.dfs(target, 0)
+        return self.res
+
+    def dfs(self, target: int, start: int):
+        if target == 0:
+            self.res.append(self.path.copy())
+        for i in range(start, self.l):
+            num = self.candidates[i]
+            if target - num < 0:
+                break
+            self.path.append(num)
+            self.dfs(target-num, i)
+            self.path.pop(-1)
