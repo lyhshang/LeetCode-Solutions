f497d7c93a93014183e340365eff67058ddac31b
lyhshang <397504018@qq.com>
2020-05-16 13:14:57 +0800

    0042 update

--- Solutions/0042.trap.py
+++ Solutions/0042.trap.py
@@ -18,20 +18,41 @@ from typing import List
 
 class Solution:
     def trap(self, height: List[int]) -> int:
+        # 栈
         q = []
         lh = 0
         res = 0
         for i in range(len(height)):
-            while len(q)>0 and height[i] >= q[-1][0]:
-                res += (i-q[-1][1]-1)*(q[-1][0]-lh)
+            while len(q) > 0 and height[i] >= q[-1][0]:
+                res += (i - q[-1][1] - 1) * (q[-1][0] - lh)
                 lh = q[-1][0]
                 q.pop(-1)
-            if len(q)>0:
+            if len(q) > 0:
                 res += (i - q[-1][1] - 1) * (height[i] - lh)
             q.append((height[i], i))
             lh = height[i]
         return res
 
+    def trap2(self, height: List[int]) -> int:
+        # 双指针，空间复杂度O(1)
+        l, r = 0, len(height) - 1
+        res = 0
+        lmax, rmax = 0, 0
+        while l <= r:
+            if height[l] <= height[r]:
+                if height[l] > lmax:
+                    lmax = height[l]
+                else:
+                    res += lmax - height[l]
+                l += 1
+            else:
+                if height[r] > rmax:
+                    rmax = height[r]
+                else:
+                    res += rmax - height[r]
+                r -= 1
+        return res
+
 
 if __name__ == '__main__':
-    print(Solution().trap([0,1,0,2,1,0,1,3,2,1,2,1]), 6)
+    print(Solution().trap2([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]), 6)
